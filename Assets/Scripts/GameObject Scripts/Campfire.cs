559adc464bd8336c76540e908945bab7c6c698a0
LightNeverDies <LightNeverDies@abv.bg>
2020-03-21 00:23:22 +0200

    Campfire Partical System Fix

--- Assets/Scripts/GameObject Scripts/Campfire.cs
+++ Assets/Scripts/GameObject Scripts/Campfire.cs
@@ -8,7 +8,7 @@ public class Campfire : MonoBehaviour
     public ParticleSystem Smoke;
 
     public hud Hud;
-    public GameObject campfire;
+    private bool _isTrigger= false;
 
     void Start()
     {
@@ -19,40 +19,45 @@ public class Campfire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(campfire != null &&Input.GetKeyDown(KeyCode.E))
+        
+        /*if(Input.GetKeyDown(KeyCode.E))
+        {
+            Camp();
+        }*/
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Camp();
         }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(campfire != null)
-        {
             Hud.OpenHint("");
-        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (campfire != null)
-        {
             Hud.CloseHint();
-        }
     }
+
+
 
     private void Camp()
     {
-       if (Fire.isPlaying && Smoke.isPlaying)
-       {
-         Fire.Stop();
-         Smoke.Stop();
-         Debug.Log("in?");
-       }
-       else
-       {
-         Fire.Play();
-         Smoke.Play();
-       }
+        if (Fire.isPlaying && Smoke.isPlaying)
+        {
+            Fire.Stop();
+            Smoke.Stop();
+        }
+        else
+        {
+            Fire.Play();
+            Smoke.Play();
+        }
     }
 
 
